4ed1b8329a4e585c413bf0e97add308a2d1ed292
Yeonwoo <kyu@whatap.io>
2026-02-03 23:58:44 +0900

    feat: add list command

---
+++ cmd/list.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/kyw/ts/internal/tmux"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all tmux sessions",
+	RunE:  runList,
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	if !tmux.IsInstalled() {
+		return fmt.Errorf("tmux not found")
+	}
+
+	sessions, err := tmux.ListSessions()
+	if err != nil {
+		return err
+	}
+
+	if len(sessions) == 0 {
+		fmt.Println("no sessions")
+		return nil
+	}
+
+	for _, s := range sessions {
+		fmt.Println(s.Name)
+	}
+
+	return nil
+}
